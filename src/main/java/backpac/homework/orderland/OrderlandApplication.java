1da5014b7a8a6313128bfc2e06cf7e24e2758526
Timo <developer.timo@gmail.com>
2021-05-07 15:37:56 +0900

    initialize

---
+++ src/main/java/backpac/homework/orderland/OrderlandApplication.java
@@ -0,0 +1,13 @@
+package backpac.homework.orderland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderlandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderlandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backpac/homework/orderland/OrderlandApplicationTests.java
@@ -0,0 +1,13 @@
+package backpac.homework.orderland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderlandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
