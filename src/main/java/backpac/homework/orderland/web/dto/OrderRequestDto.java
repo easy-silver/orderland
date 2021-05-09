ec33dfd603fa50c06c3393c64de35371f0029fbe
Timo <developer.timo@gmail.com>
2021-05-10 01:32:57 +0900

    주문 관련 DTO 생성

---
+++ src/main/java/backpac/homework/orderland/web/dto/OrderRequestDto.java
@@ -0,0 +1,28 @@
+package backpac.homework.orderland.web.dto;
+
+import backpac.homework.orderland.domain.order.Order;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter @Getter
+public class OrderRequestDto {
+
+    private String productName;
+    private LocalDateTime paymentDate;
+    private Long memberNo;
+
+    @Builder
+    public OrderRequestDto(String productName, LocalDateTime paymentDate, Long memberNo) {
+        this.productName = productName;
+        this.paymentDate = paymentDate;
+        this.memberNo = memberNo;
+    }
+
+    public Order toEntity() {
+        return Order.builder()
+                .build();
+    }
+}
