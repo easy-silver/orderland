b5baa492422d0a1cddca4ed0cfe5d29ad9efb3aa
Timo <developer.timo@gmail.com>
2021-05-09 22:57:08 +0900

    회원별 주문 조회 기능 및 테스트 케이스 추가

--- src/test/java/backpac/homework/orderland/domain/order/OrderRepositoryTest.java
+++ src/test/java/backpac/homework/orderland/domain/order/OrderRepositoryTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -49,5 +50,58 @@ public class OrderRepositoryTest {
         Order findOrder = orderRepository.findById(order.getOrderNo()).get();
         assertThat(findOrder.getProductName()).isEqualTo("새우깡");
         assertThat(findOrder.getMember()).isEqualTo(member);
+    }
+
+    @Test
+    public void 회원별_주문_조회() {
+        //given
+        Member member1 = memberRepository.save(Member.builder()
+                .name("이지은")
+                .nickname("티모")
+                .password("!qwerasdf1234")
+                .email("timo@mail.com")
+                .telNo("01012341234")
+                .gender(Gender.FEMALE)
+                .build());
+
+        orderRepository.save(Order.builder()
+                .orderNo("ORDER0011111")
+                .productName("새우깡")
+                .paymentDate(LocalDateTime.now())
+                .member(member1)
+                .build());
+
+        orderRepository.save(Order.builder()
+                .orderNo("ORDER0022222")
+                .productName("구운감자")
+                .paymentDate(LocalDateTime.now())
+                .member(member1)
+                .build());
+
+        Member member2 = memberRepository.save(Member.builder()
+                .name("이지금")
+                .nickname("모티")
+                .password("!qwerasdf1234")
+                .email("moti@mail.com")
+                .telNo("01012341235")
+                .gender(Gender.MALE)
+                .build());
+
+        orderRepository.save(Order.builder()
+                .orderNo("ORDER0033333")
+                .productName("홈런볼")
+                .paymentDate(LocalDateTime.now())
+                .member(member2)
+                .build());
+
+        //when
+        List<Order> all = orderRepository.findAll();
+        List<Order> allByMember1 = orderRepository.findAllByMember(member1);
+        List<Order> allByMember2 = orderRepository.findAllByMember(member2);
+
+        //then
+        assertThat(all.size()).isEqualTo(3);
+        assertThat(allByMember1.size()).isEqualTo(2);
+        assertThat(allByMember2.size()).isEqualTo(1);
     }
 }
