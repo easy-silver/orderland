7d2d90913457d88694a7ec435a4406e03fafe666
Timo <developer.timo@gmail.com>
2021-05-17 22:33:37 +0900

    테스트 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,9 @@
+-- MEMBER(회원)
 INSERT INTO MEMBER (NAME, NICKNAME, PASSWORD, TEL_NO, EMAIL, GENDER)
-            VALUES ('이지은', '티모', '!qwerasdf1234', '01012341234', 'timo@gmailc.om', 'FEMALE');
+            VALUES ('이지은', '티모', '!qwerasdf1234', '01012341234', 'timo@gmail.com', 'FEMALE');
+INSERT INTO MEMBER (NAME, NICKNAME, PASSWORD, TEL_NO, EMAIL, GENDER)
+            VALUES ('이라떼', '라떼', '!qwerasdf5678', '01012341234', 'latte@gmail.com', 'ETC');
+
+-- ORDERS(주문)
+INSERT INTO ORDERS(ORDER_NO, PAYMENT_DATE, PRODUCT_NAME, MEMBER_NO)
+            VALUES('TEST00001234', SYSDATE, '새우깡', 1);
